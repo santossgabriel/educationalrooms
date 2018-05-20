6bbd4ef031a6e2a87dee00bce8690abe9f53fdb0
Gabriel Rosa <gabrielsantoscimol@gmail.com>
2018-05-20 12:29:32 -0300

    ADD - angular router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component'
 import { ToolbarComponent } from './toolbar/toolbar.component'
 import { FooterComponent } from './footer/footer.component'
 import { HomeComponent } from './home/home.component'
-import { AppMaterialModule } from './app-material.module';
+import { AboutComponent } from './about/about.component'
+import { AppMaterialModule } from './app-material.module'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
