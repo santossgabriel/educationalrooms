e6ce9fb6d7b13405b7d8d9593978e2b37370b22c
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2018-07-27 20:29:02 -0300

    Ajustes de layout

--- src/run.js
+++ src/run.js
@@ -2,6 +2,4 @@ import app from './server'
 
 const port = process.env.PORT || process.env.NODE_PORT || 80
 
-console.log(`Environment: ${process.env.NODE_ENV}`)
-
 app.listen(port, () => console.log(`Listening in port ${port}`))
