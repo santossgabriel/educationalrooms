c0d51a00edc31be3213633b649518402f9c7df6b
Gabriel Santos <gabrielsantoscimol@gmail.com>
2020-05-19 21:05:35 -0300

    MainComponent Refactory
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- client/react/src/components/index.js
+++ client/react/src/components/index.js
@@ -1,26 +1,18 @@
-export { BrazilFlag, UnitedStatesFlag } from './main/Flags'
+export { default as Toolbar } from 'components/Toobar/Toolbar'
+export { default as Footer } from './Footer/Footer'
+export { default as OpenedQuizLinkList } from './Footer/OpenedQuizLinkList'
+export { default as GlobalToast } from './GlobalToast/GlobalToast'
+export { default as InputHookForm } from './hook-form/InputHookForm'
 export { default as AppRouter } from './main/AppRouter'
 export { default as CardMain } from './main/CardMain'
+export { default as FadeInContainer } from './main/FadeInContainer'
+export { BrazilFlag, UnitedStatesFlag } from './main/Flags'
 export { default as GoogleButton } from './main/GoogleButton'
-export {
-  ErrorAnimatedIcon, SuccessAnimatedIcon, ArrowLeft,
-  ArrowRight, ArrowUp, ArrowDown
-} from './main/Icons'
+export { ArrowDown, ArrowLeft, ArrowRight, ArrowUp, ErrorAnimatedIcon, SuccessAnimatedIcon } from './main/Icons'
 export { default as IconTextInput } from './main/IconTextInput'
 export { default as MainComponen } from './main/MainComponent'
 export { AlertModal, ConfirmModal } from './main/Modal'
-export { default as Toolbar } from 'components/Toobar/Toolbar'
-export { default as FadeInContainer } from './main/FadeInContainer'
-export { default as Footer } from './Footer/Footer'
-export { default as OpenedQuizLinkList } from './Footer/OpenedQuizLinkList'
-
 export { default as EditQuestionModal } from './modais/EditQuestionModal'
-// export { default as EditUserModal } from './modais/EditUserModal'
 export { default as SelectQuestionModal } from './modais/SelectQuestionModal'
-
 export { default as EditQuestionAlternatives } from './question/EditQuestionAlternatives'
 export { default as Stars } from './question/Stars'
-
-export { default as InputHookForm } from './hook-form/InputHookForm'
-
-export { default as GlobalToast } from './GlobalToast/GlobalToast'
